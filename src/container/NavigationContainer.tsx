740ff505e587e84ba4e30af487555ceb26bd5fd1
HanTaeyeong <gksxodud2721@gmail.com>
2020-08-03 18:01:12 +0900

    NavContainer-base

--- src/container/NavigationContainer.tsx
+++ src/container/NavigationContainer.tsx
@@ -1,4 +1,17 @@
 import React from 'react';
 
 
+const NavigationContainer = () => {
 
+    return <div>
+
+        <div>Intro</div>
+        <div>Stack</div>
+        <div>Career</div>
+        <div>Contact</div>
+    
+    </div>
+
+}
+
+export default NavigationContainer;
